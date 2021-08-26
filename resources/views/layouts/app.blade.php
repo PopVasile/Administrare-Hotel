6eb56a85f3e0455e1babecd53d6d5e2271074623
unknown <hosuioan6@gmail.com>
2021-08-26 22:49:26 +0300

    Administration Hotel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,38 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style>
+    .nav-brand
+    {
+        position: relative;
+        right: 200px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 20px;
+    }
+    .image-brand
+    {
+        position: relative;
+        right: 210px;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 20px;
 
+    }
+    img{
+        height: 50px;
+        width: 50px;
+    }
+</style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <div class="image-brand">
+                    <img src="https://images.cdn2.stockunlimited.net/clipart450/hotel_1532808.jpg">
+                </div>
+                <div class="nav-brand">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
